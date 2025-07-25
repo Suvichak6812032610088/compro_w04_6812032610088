fedaa609367b146e6bd0b21a1f01bd6c132dea60
Suvichak6812032610088 <s6812032610088@email.kmutnb.ac.th>
2025-07-25 13:32:46 +0700

    Add files via upload

---
+++ Coding_w04-01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){                 //int main Keyword เป็นข้อมูลชนิดของฟังก์ชัน main () คือ operator
+    printf("Hello,\n");     // Identifier คือ printf,Constant คือ "Hello,\n"
+    printf("C Tokens!\n");  // Constant คือ "C tokens!\n"
+    return 0;               // Keyword คือ return,Constant :0
+}
